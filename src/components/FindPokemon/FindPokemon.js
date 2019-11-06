26fcd6828a59d4b400322b0ff203f26eea05616f
Pii <pii@Piis-iMac.local>
2019-11-05 23:36:03 -0300

    Consumo da Api

--- src/components/FindPokemon/FindPokemon.js
+++ src/components/FindPokemon/FindPokemon.js
@@ -1,25 +1,51 @@
 import React, { useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Page, Form, Input, FormButton, Linear } from './FindPokemon.style';
+import api from '../../clients/pokeApi';
+import {
+  Page,
+  Form,
+  Input,
+  FormButton,
+  Linear,
+  Logo,
+  Result,
+  Text,
+} from './FindPokemon.style';
 
 export default () => {
   const [primaryBgColor, setPrimaryBgColor] = useState('#fff');
   const [secondBgColor, setSecondBgColor] = useState('#fff');
+  const [namePokemon, setNamePokemon] = useState('');
+
+  const findByPokemon = async () => {
+    const response = await api.get(`/pokemon/${namePokemon}`);
+  };
+
+  const handleClick = () => {
+    findByPokemon();
+  };
 
   return (
     <Linear colors={[primaryBgColor, secondBgColor]}>
       <Page>
+        <Logo
+          source={require('../../assets/images/logo.png')}
+          resizeMode="stretch"
+        />
         <Form>
           <Input
             placeholder="Digite o Pokemon"
             autoCapitalize="none"
             autoCompleteType="off"
             autoCorrect={false}
+            value={namePokemon}
+            onChangeText={name => setNamePokemon(name)}
           />
-          <FormButton>
+          <FormButton onPress={handleClick}>
             <Icon name="search" size={30} color="#fff" />
           </FormButton>
         </Form>
+        <Result />
       </Page>
     </Linear>
   );
--- src/components/FindPokemon/FindPokemon.style.js
+++ src/components/FindPokemon/FindPokemon.style.js
@@ -1,21 +1,30 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
 
+export const Linear = styled(LinearGradient)`
+  flex: 1;
+`;
+
 export const Page = styled.SafeAreaView`
   align-items: center;
+`;
+
+export const Logo = styled.Image`
+  width: 90%;
+  margin-top: 20px;
+  height: 100px;
 `;
 
 export const Form = styled.View`
   flex-direction: row;
   width: 90%;
   justify-content: space-between;
-  margin-top: 50px;
+  margin-top: 20px;
 `;
 
 export const Input = styled.TextInput`
-  width: 50px;
   height: 50px;
-  border: 1px solid #6b6b6b;
+  border: 1px solid #3b5ca5;
   border-radius: 5px;
   width: 75%;
   font-size: 18px;
@@ -26,14 +35,22 @@ export const FormButton = styled.TouchableOpacity`
   width: 20%;
   height: 50px;
   border-radius: 5px;
-  background-color: #0017ff;
+  background-color: #3b5ca5;
   align-items: center;
   justify-content: center;
 `;
 
-export const Linear = styled(LinearGradient)`
-  flex: 1;
+export const Result = styled.View`
+  width: 90%;
+  height: 300px;
+  box-shadow: 0px 5px 5px #c8c8c8;
+  background-color: #fff;
+  border-radius: 10px;
+  margin-top: 100px;
+  elevation: 10;
 `;
+
+export const Result2 = styled.View``;
 
 export const Text = styled.Text`
   font-size: 18px;
