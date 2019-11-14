e614d5528ebe72f4159a4ab1f3cad27f3b7fd56a
Pii <pii@Piis-iMac.local>
2019-11-14 13:55:59 -0300

    Correções vulnerabilidade

--- src/components/FindPokemon/FindPokemon.js
+++ src/components/FindPokemon/FindPokemon.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Keyboard, ActivityIndicator, StatusBar } from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../clients/pokeApi';
 import {
@@ -10,7 +10,6 @@ import {
   Linear,
   Logo,
   Result,
-  Text,
   Img,
   Status,
   ResultInfoPoke,
